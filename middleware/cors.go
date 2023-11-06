567fd5b109304fd9985bd977a1ab1158e322d373
Suresh.Konar <suresh.konar@connectwise.com>
2023-11-06 15:05:50 +0530

    added netlify url in cors

--- middleware/cors.go
+++ middleware/cors.go
@@ -6,7 +6,7 @@ import (
 
 func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173,https://fascinating-cendol-045874.netlify.app")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
